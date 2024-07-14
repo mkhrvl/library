dd0e6aace665edc67609fd10af3711a12e430020
Michael Ken Revil <60070288+mkhrvl@users.noreply.github.com>
2024-07-14 13:38:11 +0800

    feat: create function to add book entry to library

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book(author, title, pages, isRead) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary(author, title, pages, isRead) {
+    const bookEntry = new Book(author, title, pages, isRead);
+    myLibrary.push(bookEntry);
+}
